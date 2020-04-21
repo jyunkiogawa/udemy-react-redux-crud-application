9d7763e7a54ae9555abd65fb93879dab925ac25f
小川純輝 <jyunki9780@gmail.com>
2020-04-21 09:54:15 +0900

    implement events index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,18 @@
-export const INCREMENT = 'INCREMENT'
-export const DECREMENT = 'DECREMENT'
+// アクション
+import axios from 'axios'
 
+export const READ_EVENTS = 'READ_EVENTS'
 
-export const increment = () =>({
-    type: INCREMENT
-})
+// イベントのcrud処理に共通で使うものなのでここで定義しておく
+const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
+// curlコマンドでapiサーバーと通信した時と同じもの
+const QUERYSTRING = '?token=token123'
 
-export const decrement = () =>({
-    type: DECREMENT
-})
+// 非同期処理を実装
+export const readEvents = () => async dispatch => {
+  // axiosnによるhttpリクエストの送信
+  // 戻り値が非同期処理（promise)になってしまうのでawaitを使う
+  // const response = await axios.get(`${ROOT_URL}/evemts${QUERYSTRING}`)
+  const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
+  dispatch({ type: READ_EVENTS, response })
+}
---
+++ src/components/events_index.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+// これがcontainer的な役割かも
+// これでstoreとAppコンポーネントを接続
+// これにより、store内のstateが参照できるようになります
+import _ from 'lodash'
+
+
+import { readEvents } from '../actions'
+
+class EventsIndex extends Component {
+  componentDidMount() {
+    // ここで外部のapiサーバーよりデータを取得
+    this.props.readEvents()
+  }
+
+  renderEvents() {
+    return _.map(this.props.events, event => (
+      <tr key ={event.id}>
+        <td>{event.id}</td>
+        <td>{event.title}</td>
+        <td>{event.body}</td>
+      </tr>
+    ))
+  }
+
+  render(){
+    return (
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Body</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {this.renderEvents()}
+          </tbody>
+        </table>
+    )
+  }
+}
+
+const mapStateToProps = state => ({ events: state.events })
+// これによりvalueを受け取れる
+
+const mapDispatchToProps = ({ readEvents })
+// これにより遷移が可能になる
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex)
+
+
+// export default App;
