df9440d186d15a38450216b787532b12d956adf7
小川純輝 <jyunki9780@gmail.com>
2020-04-21 20:26:01 +0900

    implement event new component

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,8 @@
 import axios from 'axios'
 
 export const READ_EVENTS = 'READ_EVENTS'
+export const CREATE_EVENT = 'CREATE_EVENT'
+
 
 // イベントのcrud処理に共通で使うものなのでここで定義しておく
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
@@ -15,4 +17,9 @@ export const readEvents = () => async dispatch => {
   // const response = await axios.get(`${ROOT_URL}/evemts${QUERYSTRING}`)
   const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
   dispatch({ type: READ_EVENTS, response })
+}
+
+export const postEvent = values => async dispatch => {
+  const response = await axios.post(`${ROOT_URL}/events${QUERYSTRING}`, values)
+  dispatch({ type: CREATE_EVENT, response })
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,6 @@
 import {  combineReducers } from 'redux'
+// 名前が一般的すぎるので変更
+import { reducer as form } from 'redux-form'
 import events from './events'
 
-export default combineReducers({ events })
+export default combineReducers({ events, form })
